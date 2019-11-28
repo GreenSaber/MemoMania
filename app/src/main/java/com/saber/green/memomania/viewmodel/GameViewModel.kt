db5feba6df38c671330e7b2b8bba0211ef14e98f
molotkovaa <artem.molotkov@oxagile.com>
2019-11-28 18:23:45 +0300

    mvvm fixed

--- app/src/main/java/com/saber/green/memomania/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/saber/green/memomania/viewmodel/GameViewModel.kt
@@ -7,9 +7,22 @@ import com.saber.green.memomania.model.Tile
 
 class GameViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var gameLevel : Int = 1
+    private var activeTiles : ArrayList<Tile> = Game(getGameLevel()).getActiveTiles()!!
     private lateinit var game: Game
 
-    fun getActiveTiles(): ArrayList<Tile> {
-        return Game(1).getActiveTiles()!!
+
+    fun setGameLevel(gameLevelNumber : Int){
+        gameLevel =  gameLevelNumber
     }
+
+    fun getGameLevel(): Int {
+        return gameLevel
+    }
+
+    fun getActiveTiles(): ArrayList<Tile> {
+        return activeTiles
+    }
+
+
 }
