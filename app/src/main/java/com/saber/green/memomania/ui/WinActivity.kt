7cb38e2fb1ab641fbe8e0d1dc35532faf0bb2ff8
GreenSaber <GreenZabimaru@gmail.com>
2020-01-10 03:27:19 +0300

    ui fixed

--- app/src/main/java/com/saber/green/memomania/ui/WinActivity.kt
+++ app/src/main/java/com/saber/green/memomania/ui/WinActivity.kt
@@ -3,15 +3,22 @@ package com.saber.green.memomania.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.saber.green.memomania.R
+import com.saber.green.memomania.viewmodel.WinViewModel
 import kotlinx.android.synthetic.main.activity_win.*
 
 class WinActivity : AppCompatActivity() {
 
+    private lateinit var winViewModel: WinViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win)
+        winViewModel = ViewModelProviders.of(this).get(WinViewModel::class.java)
+
         onHomeButtonPressed()
+        onRestartButtonPressed()
     }
 
     fun onHomeButtonPressed() {
@@ -19,6 +26,15 @@ class WinActivity : AppCompatActivity() {
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_left)
+        }
+    }
+
+    fun onRestartButtonPressed() {
+        win_restart_button.setOnClickListener {
+            winViewModel.resetGame()
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_right)
         }
     }
 
