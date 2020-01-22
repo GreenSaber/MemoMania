45c88a67e32135ad7d987920a291361c1d27313a
molotkovaa <artem.molotkov@oxagile.com>
2020-01-22 13:40:17 +0300

    fragment refactoring

--- app/src/main/java/com/saber/green/memomania/ui/GameActivity.kt
+++ app/src/main/java/com/saber/green/memomania/ui/GameActivity.kt
@@ -1,49 +1,35 @@
 package com.saber.green.memomania.ui
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.button.MaterialButton
 import com.saber.green.memomania.R
-import com.saber.green.memomania.model.GameLifecycle
 import com.saber.green.memomania.utils.AnimationUtils
 import com.saber.green.memomania.utils.VibrationUtils
 import com.saber.green.memomania.viewmodel.GameViewModel
 import kotlinx.android.synthetic.main.activity_game.*
 import java.util.*
-import kotlin.collections.ArrayList
 
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(),
+    GameFieldClassicFragment.GameFieldClassicFragmentListener {
 
-    private val TAG = "GameActivity"
-    private val INITIAL_SHOW_DELAY : Long = 2000
     private lateinit var gameViewModel: GameViewModel
     private lateinit var vibrationUtils: VibrationUtils
-
-    private val activeButtons = ArrayList<MaterialButton>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         gameViewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-        vibrationUtils = VibrationUtils(this)
-
-        initActiveButtons()
-        highlightActiveButtons(this)
-        showValueOfActiveButtons(this)
-        hideValueOfActiveButtons(this)
+        val fragment: GameFieldClassicFragment = GameFieldClassicFragment.newInstance()
+        supportFragmentManager.beginTransaction().replace(R.id.grid_layout_container, fragment)
+            .commit()
 
         initLevelObserver()
         initLifeObserver()
-
-        onActiveButtonClick()
         onHomeButtonClick()
         onRestartButtonClick()
     }
@@ -66,121 +52,6 @@ class GameActivity : AppCompatActivity() {
         })
     }
 
-    fun initActiveButtons() {
-        val activeTiles = gameViewModel.getActiveTiles()
-        Log.i(TAG, "Tiles size : ${activeTiles.size}")
-        activeTiles.forEach { Log.i(TAG, "Tile [number : ${it.getNumber()}; value : ${it.getValue()}]") }
-        activeTiles.forEach {
-            activeButtons.add(findViewById(resources.getIdentifier("gameTileButton${it.getNumber()}", "id", packageName)))
-        }
-    }
-
-    fun highlightActiveButtons(context: Context) {
-        activeButtons.forEach { it.setBackgroundColor(ContextCompat.getColor(context, R.color.accent_color)) }
-        activeButtons.forEach { it.isEnabled = false }
-    }
-
-    fun showValueOfActiveButtons(context: Context) {
-        Timer(false).schedule(object : TimerTask() {
-            override fun run() {
-                runOnUiThread {
-                    activeButtons.forEach { it.setBackgroundColor(ContextCompat.getColor(context, R.color.dark_button_color)) }
-                    activeButtons.forEach { it.text = getButtonValue(it) }
-                    activeButtons.forEach { it.isEnabled = false }
-                }
-            }
-        }, INITIAL_SHOW_DELAY)
-    }
-
-    fun hideValueOfActiveButtons(context: Context) {
-        Timer(false).schedule(object : TimerTask() {
-            override fun run() {
-                runOnUiThread {
-                    activeButtons.forEach { it.setBackgroundColor(ContextCompat.getColor(context, R.color.accent_color)) }
-                    activeButtons.forEach { it.text = "" }
-                    activeButtons.forEach { it.isEnabled = true }
-                }
-            }
-        }, INITIAL_SHOW_DELAY + gameViewModel.getShowTiming())
-    }
-
-    fun onActiveButtonClick() {
-        activeButtons.forEach { it ->
-            it.setOnClickListener {
-                val button: MaterialButton = findViewById(it.id)
-                val buttonValue = getButtonValue(button)
-
-                when (gameViewModel.getGameLifecycle(buttonValue)) {
-
-                    GameLifecycle.CORRECT_VALUE -> {
-                        vibrationUtils.correctValueVibration()
-                        AnimationUtils.viewThreeColorAnimation(this, button, R.color.accent_color, R.color.green, R.color.dark_button_color, 2 * AnimationUtils.DURATION)
-                        AnimationUtils.scaleAnimation(button, 1.07f, AnimationUtils.DURATION)
-                        button.text = buttonValue
-                        button.isClickable = false
-                    }
-
-                    GameLifecycle.INCORRECT_VALUE -> {
-                        vibrationUtils.inCorrectValueVibration()
-                        AnimationUtils.viewTwoColorAnimation(this, button, R.color.accent_color, R.color.red, AnimationUtils.INCORRECT_DURATION, 1)
-                        AnimationUtils.layoutColorAnimation(this, life_card.background as GradientDrawable, R.color.accent_color, R.color.red, AnimationUtils.INCORRECT_DURATION)
-                        AnimationUtils.scaleAnimation(heart_icon, 1.5f, AnimationUtils.INCORRECT_DURATION)
-                        AnimationUtils.scaleAnimation(button, 1.07f, AnimationUtils.INCORRECT_DURATION)
-                    }
-
-                    GameLifecycle.NEXT_LEVEL -> {
-                        vibrationUtils.correctValueVibration()
-                        AnimationUtils.viewThreeColorAnimation(this, button, R.color.accent_color, R.color.green, R.color.dark_button_color, 2 * AnimationUtils.DURATION)
-                        AnimationUtils.scaleAnimation(button, 1.07f, AnimationUtils.DURATION)
-                        button.text = buttonValue
-                        button.isClickable = false
-
-                        val intent = Intent(this, NextLevelActivity::class.java)
-                        Timer(false).schedule(object : TimerTask() {
-                            override fun run() {
-                                runOnUiThread {
-                                    startActivity(intent)
-                                    overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_left)
-                                }
-                            }
-                        }, AnimationUtils.DURATION)
-                    }
-
-                    GameLifecycle.WIN -> {
-                        vibrationUtils.correctValueVibration()
-                        AnimationUtils.viewThreeColorAnimation(this, button, R.color.accent_color, R.color.green, R.color.dark_button_color, 2 * AnimationUtils.DURATION)
-                        AnimationUtils.scaleAnimation(button, 1.07f, AnimationUtils.DURATION)
-                        button.text = buttonValue
-                        button.isClickable = false
-                        val intent = Intent(this, WinActivity::class.java)
-                        Timer(false).schedule(object : TimerTask() {
-                            override fun run() {
-                                runOnUiThread { startActivity(intent) }
-                            }
-                        }, AnimationUtils.DURATION)
-                    }
-
-                    GameLifecycle.GAME_OVER -> {
-                        vibrationUtils.inCorrectValueVibration()
-                        AnimationUtils.viewTwoColorAnimation(this, button, R.color.accent_color, R.color.red, 2 * AnimationUtils.DURATION, 1)
-                        AnimationUtils.layoutColorAnimation(this, life_card.background as GradientDrawable, R.color.accent_color, R.color.red, 2 * AnimationUtils.DURATION)
-                        AnimationUtils.scaleAnimation(heart_icon, 1.5f, AnimationUtils.INCORRECT_DURATION)
-                        AnimationUtils.scaleAnimation(button, 1.07f, AnimationUtils.INCORRECT_DURATION)
-
-                        val intent = Intent(this, GameOverActivity::class.java)
-                        Timer(false).schedule(object : TimerTask() {
-                            override fun run() {
-                                runOnUiThread {
-                                    startActivity(intent)
-                                    overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_left)
-                                }
-                            }
-                        }, AnimationUtils.INCORRECT_DURATION)
-                    }
-                }
-            }
-        }
-    }
 
     fun onHomeButtonClick() {
         home_button.setOnClickListener {
@@ -199,17 +70,8 @@ class GameActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
-        overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_right)
-    }
-
-    private fun getButtonValue(materialButton: MaterialButton): String {
-        val buttonNumber = materialButton.resources.getResourceName(materialButton.id)
-            .replace("${packageName}:id/gameTileButton", "").toInt()
-        val tile = gameViewModel.getActiveTiles().find { it.getNumber() == buttonNumber }
-        return tile?.getValue().toString()
+    override fun onIncorrect() {
+        AnimationUtils.layoutColorAnimation(this as AppCompatActivity, life_card.background as GradientDrawable, R.color.accent_color, R.color.red, AnimationUtils.INCORRECT_DURATION)
+        AnimationUtils.scaleAnimation(heart_icon, 1.5f, AnimationUtils.INCORRECT_DURATION)
     }
 }
--- app/src/main/java/com/saber/green/memomania/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/saber/green/memomania/viewmodel/GameViewModel.kt
@@ -5,13 +5,9 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.saber.green.memomania.model.Game
-import com.saber.green.memomania.model.GameLifecycle
-import com.saber.green.memomania.model.Tile
 
 class GameViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var rightAnswersCount: Int = 0
-    private var wrongAnswersCount: Int = 0
     private val lifeCount = MutableLiveData<String>()
     private val levelCount = MutableLiveData<String>()
 
@@ -24,51 +20,8 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
 
     fun getLevelCount(): LiveData<String> = levelCount
 
-    fun getActiveTiles(): ArrayList<Tile> {
-        return Game.getActiveTiles()!!
-    }
-
-    fun getShowTiming(): Long {
-        return Game.getShowTiming()!!
-    }
-
-    fun getGameLifecycle(value: String): GameLifecycle {
-        if (getSortedTiles()[rightAnswersCount].getValue() == value.toInt()) {
-            rightAnswersCount++
-            if (rightAnswersCount == getActiveTiles().size) {
-                val level = Game.getLevel() + 1
-                Game.setLevel(level)
-                levelCount.value = Game.getLevel().toString()
-                //TODO to change on 10 value below
-                if (Game.getLevel() < 10) {
-                    return GameLifecycle.NEXT_LEVEL
-                } else {
-                    Game.resetGame()
-                    return GameLifecycle.WIN
-                }
-            } else {
-                return GameLifecycle.CORRECT_VALUE
-            }
-        } else {
-            wrongAnswersCount++
-            val currentLifesCount = Game.getLifesCount() - 1
-            Game.setLifesCount(currentLifesCount)
-            lifeCount.value = Game.getLifesCount().toString()
-            if (Game.getLifesCount() > 0) {
-                return GameLifecycle.INCORRECT_VALUE
-            } else {
-                return GameLifecycle.GAME_OVER
-            }
-        }
-    }
-
     fun resetGame(){
         Game.resetGame()
-    }
-
-    private fun getSortedTiles(): List<Tile> {
-        return getActiveTiles().sortedWith(compareBy { it.getValue() })
-
     }
 
 }
