bd168b9d1ccc4c88162072fea6f38b8a23fd6020
molotkovaa <artem.molotkov@oxagile.com>
2020-01-29 12:21:25 +0300

    sounds fixed

--- app/src/main/java/com/saber/green/memomania/ui/WinActivity.kt
+++ app/src/main/java/com/saber/green/memomania/ui/WinActivity.kt
@@ -1,43 +1,28 @@
 package com.saber.green.memomania.ui
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.saber.green.memomania.R
 import com.saber.green.memomania.ui.game.GameActivity
-import com.saber.green.memomania.utils.SoundPool
 import com.saber.green.memomania.viewmodel.WinViewModel
 import kotlinx.android.synthetic.main.activity_win.*
-import java.util.*
+
 
 class WinActivity : AppCompatActivity() {
 
     private lateinit var viewModel: WinViewModel
-    private var winSound : Int? = null
+    private var player : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win)
-        viewModel =  ViewModelProvider(this).get(WinViewModel::class.java)
-        initSounds()
+        viewModel = ViewModelProvider(this).get(WinViewModel::class.java)
         playSound()
         onHomeButtonPressed()
         onRestartButtonPressed()
-    }
-
-    fun initSounds(){
-        winSound = SoundPool.getInstance()!!.load(applicationContext, R.raw.win_sound, 1)
-    }
-
-    fun playSound(){
-        if (viewModel.getSoundStatus().value!!) {
-            Timer(false).schedule(object : TimerTask() {
-                override fun run() {
-                    runOnUiThread { SoundPool.getInstance()!!.play(winSound!!, 1F, 1F, 0, 0, 1F) }
-                }
-            }, 500)
-        }
     }
 
     fun onHomeButtonPressed() {
@@ -64,5 +49,29 @@ class WinActivity : AppCompatActivity() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_left)
+    }
+
+    fun playSound() {
+        if (viewModel.getSoundStatus().value!!) {
+            if (player == null) {
+                player = MediaPlayer.create(this, R.raw.win_sound)
+                player?.setOnCompletionListener {
+                    stopPlayer()
+                }
+            }
+            player?.start()
+        }
+    }
+
+    private fun stopPlayer() {
+        if (player != null) {
+            player!!.release()
+            player = null
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopPlayer()
     }
 }
--- app/src/main/java/com/saber/green/memomania/utils/SoundPool.kt
+++ app/src/main/java/com/saber/green/memomania/utils/SoundPool.kt
@@ -10,7 +10,7 @@ object SoundPool {
     fun getInstance(): SoundPool? {
         if (instance == null) {
             val audioAttributes: AudioAttributes = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_GAME)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).build()
             instance = SoundPool.Builder()
                 .setMaxStreams(50).setAudioAttributes(audioAttributes).build()
